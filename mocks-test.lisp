6dd84a51e891957926d88a3537c7fccbdf106160
Thayne McCombs <astrothayne@gmail.com>
2015-04-04 17:48:43 -0600

    Update tests
    Also moved defsystem for test system.

--- mocks-test.lisp
+++ mocks-test.lisp
@@ -7,9 +7,12 @@
 
 (in-package :bytecurry.mocks.test)
 
-(def-suite :bytecurry.mocks.test)
+(def-suite :bytecurry.mocks)
 
-(in-suite :bytecurry.mocks.test)
+(def-suite :bytecurry.mocks/with-mocked-functions :in :bytecurry.mocks.test)
+(def-suite :bytecurry.mocks/with-added-methods :in :bytecurry.mocks.test)
+
+(in-suite :bytecurry.mocks/with-mocked-functions)
 
 ;;; Set up some  test functions
 
@@ -53,3 +56,33 @@
 (test using-params
   (with-mocked-functions ((plus (a b) (cons a b)))
     (is (equal '(4 . 5) (plus 4 5)))))
+
+(in-suite :bytecurry.mocks/with-added-methods)
+
+(defgeneric gtest (a))
+
+(defmethod gtest ((a t))
+  "Foo")
+
+(defmethod gtest ((a string))
+  "A String")
+
+(test new-temporary-method
+  (with-added-methods ((gtest ((a (eql :foo)))
+                              (declare (ignorable a))
+                              :bar))
+    (is (eq :bar (gtest :foo)))
+    (is (string= "Foo" (gtest 5)))))
+
+(test override-method
+  (with-added-methods ((gtest ((a string))
+                              a))
+    (is (string= "Bar" (gtest "Bar")))
+    (is (string= "Test" (gtest "Test"))))
+  (skip "not implemented yet" (string= "A String" (gtest "Bar"))))
+
+(test remove-method-afterwards
+  (with-added-methods ((gtest ((a (eql :foo)))
+                              :bar))
+    (is (eq :bar (gtest :foo))))
+  (is (eq "Foo" (gtest :foo))))
